253eed7baa133d7198fdd4a03eceb7c827eaa3b7
Abdullah Clute <Abdullah.Clute@tdstelecom.com>
2017-09-15 11:11:22 -0500

    Added checkbox hide/show feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -75,3 +75,14 @@ addForm.addEventListener('submit', function(e) {
     li.appendChild(deleteBtn);
     list.appendChild(li);
 });
+
+// Hide books
+const hideBox = document.querySelector('#hide');
+
+hideBox.addEventListener('change', function(e) {
+    if (hideBox.checked) {
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'initial';
+    }
+});
